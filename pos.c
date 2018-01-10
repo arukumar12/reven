13facb68ab95cc0e45f6c6d28bd2c181b97d8f95
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-01-10 09:29:09 -0800

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+	int n;
+	printf("enter n.o");
+	scanf("%d",&n);
+	if(n<0)
+	{
+	    printf("neg");
+	}
+	else if(n==0)
+	{
+	    printf("zero");
+	}
+	else
+	{
+	    printf("postive");
+	}
+	
+	}
+	return 0;
+}
